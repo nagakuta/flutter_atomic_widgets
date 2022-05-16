f7f2e2e6f94be542b26f025f60a6a7dcd86f4098
nagakuta <nagakuta0314+github@gmail.com>
2022-05-16 19:38:17 +0900

    :sparkles: Implement Atomic widgets

--- lib/atomic_widget.dart
+++ lib/atomic_widget.dart
@@ -1,1 +1,5 @@
-
+export "src/interface/atoms/widget.dart";
+export 'src/interface/molecules/widget.dart';
+export 'src/interface/organisms/widget.dart';
+export 'src/interface/pages/widget.dart';
+export 'src/interface/templates/widget.dart';
---
+++ lib/src/interface/atoms/widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+
+/// Atom Widget
+abstract class AtomWidget extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const AtomWidget({
+    final Key? key,
+    final EdgeInsetsGeometry? padding,
+    final EdgeInsetsGeometry? margin,
+  })  : _padding = padding,
+        _margin = margin,
+        super(key: key);
+
+  final EdgeInsetsGeometry? _padding;
+  final EdgeInsetsGeometry? _margin;
+
+  /// Build a material widget.
+  Widget buildMaterial(final BuildContext context);
+
+  /// Build a cupertino widget.
+  Widget? buildCupertino(final BuildContext context) => null;
+
+  /// Build a widget with wrapper.
+  Widget? buildWrapper(final BuildContext context, final Widget? child) =>
+      Container(
+        padding: _padding,
+        margin: _margin,
+        child: child,
+      );
+
+  @override
+  Widget build(final BuildContext context) {
+    final Widget? cupertinoWidget = buildCupertino(context);
+    final Widget materialWidget = buildMaterial(context);
+
+    final Widget child = CupertinoUserInterfaceLevel.maybeOf(context) != null
+        ? cupertinoWidget ?? materialWidget
+        : materialWidget;
+
+    return buildWrapper(context, child) ?? child;
+  }
+}
