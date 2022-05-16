0f59c658759f86b2eef6a266fa047f3b6eb43d48
nagakuta <nagakuta0314+github@gmail.com>
2022-05-16 19:38:17 +0900

    :sparkles: Implement Atomic widgets

---
+++ lib/src/interface/molecules/widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+
+/// Molecule Widget
+abstract class MoleculeWidget extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const MoleculeWidget({
+    final Key? key,
+    final EdgeInsetsGeometry? padding,
+    final EdgeInsetsGeometry? margin,
+  })  : _padding = padding,
+        _margin = margin,
+        super(key: key);
+
+  final EdgeInsetsGeometry? _padding;
+  final EdgeInsetsGeometry? _margin;
+
+  /// Build a molecule widget.
+  Widget buildMolecule(final BuildContext context);
+
+  /// Build a widget with wrapper.
+  Widget buildWrapper(final BuildContext context, final Widget child) =>
+      Container(
+        padding: _padding,
+        margin: _margin,
+        child: child,
+      );
+
+  @override
+  Widget build(final BuildContext context) =>
+      buildWrapper(context, buildMolecule(context));
+}
---
+++ lib/src/interface/pages/widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '/src/interface/templates/widget.dart';
+
+/// Page Widget
+abstract class PageWidget extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const PageWidget({
+    final Key? key,
+  }) : super(key: key);
+
+  /// Build a scaffold body.
+  TemplateWidget buildBody(final BuildContext context);
+
+  /// Build a scaffold.
+  Scaffold buildMaterialScaffold(
+    final BuildContext context, {
+    required final TemplateWidget body,
+    final PreferredSizeWidget? appBar,
+    final Widget? floatingActionButton,
+  }) =>
+      Scaffold(
+        appBar: appBar,
+        body: body,
+        floatingActionButton: floatingActionButton,
+      );
+
+  /// Build a cupertino page scaffold.
+  CupertinoPageScaffold? buildCupertinoScaffold(
+    final BuildContext context, {
+    required final TemplateWidget child,
+    final ObstructingPreferredSizeWidget? navigationBar,
+  }) =>
+      CupertinoPageScaffold(
+        navigationBar: navigationBar,
+        child: child,
+      );
+
+  /// Build a app bar.
+  PreferredSizeWidget? buildMaterialAppBar(final BuildContext context) => null;
+
+  /// Build a cupertino navigation bar.
+  ObstructingPreferredSizeWidget? buildCupertinoNavigationBar(
+    final BuildContext context,
+  ) =>
+      const CupertinoNavigationBar();
+
+  /// Build a floating action button.
+  Widget? buildMaterialFloatingActionButton(final BuildContext context) => null;
+
+  @override
+  Widget build(final BuildContext context) {
+    final CupertinoPageScaffold? cupertinoPageScaffold = buildCupertinoScaffold(
+      context,
+      navigationBar: buildCupertinoNavigationBar(context),
+      child: buildBody(context),
+    );
+
+    final Scaffold materialScaffold = buildMaterialScaffold(
+      context,
+      appBar: buildMaterialAppBar(context),
+      body: buildBody(context),
+      floatingActionButton: buildMaterialFloatingActionButton(context),
+    );
+
+    return CupertinoUserInterfaceLevel.maybeOf(context) != null
+        ? cupertinoPageScaffold ?? materialScaffold
+        : materialScaffold;
+  }
+}
