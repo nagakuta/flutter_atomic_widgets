cde5457265b83e7cfd434c1d8b1fa9297501462d
nagakuta <nagakuta0314+github@gmail.com>
2022-05-16 19:38:17 +0900

    :sparkles: Implement Atomic widgets

---
+++ lib/src/interface/templates/widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+/// Template Widget
+abstract class TemplateWidget extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const TemplateWidget({
+    final Key? key,
+  }) : super(key: key);
+
+  /// Build a template widget.
+  Widget buildTemplate(final BuildContext context);
+
+  /// Build a safe area.
+  SafeArea buildSafeArea(final BuildContext context, final Widget child) =>
+      SafeArea(child: child);
+
+  @override
+  Widget build(final BuildContext context) =>
+      buildSafeArea(context, buildTemplate(context));
+}
