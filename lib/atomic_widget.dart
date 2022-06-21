c65377a495f59e871db86260aed745e3f6c95558
nagakuta <nagakuta0314+github@gmail.com>
2022-06-21 22:04:40 +0900

    :wrench: Export usable classes

--- lib/atomic_widget.dart
+++ lib/atomic_widget.dart
@@ -1,7 +1,10 @@
 export 'src/interface/adaptive_view//widget.dart';
 export 'src/interface/adaptive_view/types.dart';
+export 'src/interface/atoms/stateful_widget.dart';
 export "src/interface/atoms/widget.dart";
+export 'src/interface/molecules/stateful_widget.dart';
 export 'src/interface/molecules/widget.dart';
+export 'src/interface/organisms/stateful_widget.dart';
 export 'src/interface/organisms/widget.dart';
 export 'src/interface/pages/widget.dart';
 export 'src/interface/templates/widget.dart';
